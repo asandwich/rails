ff757a1bd7f7200e68d40e3a43de3e36e9f6e807
Christian Duranleau <christian.duranleau@cerner.com>
2014-08-15 09:50:57 -0500

    Updated

--- activesupport/lib/active_support/core_ext_test.rb
+++ activesupport/lib/active_support/core_ext_test.rb
@@ -1,6 +1,11 @@
 
 paths=Dir["#{File.dirname(__FILE__)}/core_ext/*.rb"]
+puts paths
+
 paths.each do |path|
-    puts path
     require path
 end
+
+
+#require 'active_support/core_ext/object'
+#require 'active_support/core_ext/numeric'
